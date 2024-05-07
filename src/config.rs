c829e435ba15ecec1bf0c654afb4f44eedb2b102
Samuel Nösslböck <samuel.noesslboeck@gmail.com>
2024-05-08 00:41:58 +0200

    fixing bugs 14

--- src/config.rs
+++ src/config.rs
@@ -8,9 +8,9 @@ use syunit::*;
 
 pub fn parse_env<F : FromStr, K : AsRef<OsStr>>(key : K) -> Result<F, syact::Error> {
     Ok(std::env::var(key).map_err(|v| {
-        format!("Failed to load from env! Var '{}' not found! Original error: {}", key, v)
+        format!("Failed to load from env! Var '{}' not found! Original error: {}", key.as_ref(), v)
     })?.parse().map_err(|v| {
-        format!("Failed to load from env! Var '{}' could not be parsed! Original error: {}", key, v)
+        format!("Failed to load from env! Var '{}' could not be parsed! Original error: {}", key.as_ref(), v)
     })?)
 }
 
